99330cc22fb4a2ce94fc7f333cb896ba1fb977bc
unknown <alexcone@live.com>
2019-01-17 19:39:04 -0600

    added a command to troll Steven :)

---
+++ commands/stevenFails.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'steven',
+    description: 'Links clips of Steven failing',
+    execute(message, args) {
+        message.channel.send("https://clips.twitch.tv/WiseSourDootNotATK");
+        message.channel.send("https://clips.twitch.tv/GleamingExpensiveFalconTF2John");
+    }
+}
